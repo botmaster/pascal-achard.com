ad1f6b6f96680167d2b111bae67aa1ef9267a917
botmaster <pascal.achard@gmail.com>
2023-02-25 18:45:37 +0100

    :tada: First commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis delectus doloremque esse et, eum fuga harum illo in inventore necessitatibus neque quaerat quam quos tempore totam veniam. Inventore, possimus?</p>
+  </div>
+</template>
