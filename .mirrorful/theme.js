df6b1187f28c9a696794e4beee1f8d2ac0f09f0e
botmaster <pascal.achard@gmail.com>
2023-04-21 19:32:10 +0200

    🚧 Feed Mirrorful colors

---
+++ .mirrorful/theme.js
@@ -0,0 +1,28 @@
+export const Tokens = {
+  colors: {
+    'my-super-color': {
+      base: '#df01df',
+    },
+    'nord-0': {
+      base: '#2e3440',
+    },
+    'nord-1': {
+      content: '#2d3340',
+      base: '#3b4252',
+    },
+    'nord-2': {
+      base: '#434c5e',
+    },
+    'nord-3': {
+      base: '#4c566a',
+    },
+    'nord-41': {
+      test: '#d12626',
+      base: '#d8dee9',
+    },
+  },
+  fontSizes: {},
+  fontWeights: {},
+  lineHeights: {},
+  boxShadows: {},
+}
--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@ onBeforeUnmount(() => {
 
 const resizeHandler = () => {
   // console.log("resize");
-  // First we get the viewport height and we multiply it by 1% to get a value for a vh unit
+  // First we get the viewport height, and we multiply it by 1% to get a value for a vh unit
   const vh = window.innerHeight * 0.01;
   // Then we set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty("--vh", `${vh}px`);
