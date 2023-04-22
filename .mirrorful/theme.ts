86239c8e53d22d3134d779db96519e235998f97e
botmaster <pascal.achard@gmail.com>
2023-04-22 11:07:00 +0200

    💄Add color vars from Mirrorful

--- .mirrorful/theme.ts
+++ .mirrorful/theme.ts
@@ -14,7 +14,6 @@
       base: '#2e3440',
     },
     'nord-1': {
-      content: '#2d3340',
       base: '#3b4252',
     },
     'nord-2': {
@@ -23,9 +22,57 @@
     'nord-3': {
       base: '#4c566a',
     },
-    'nord-41': {
-      test: '#d12626',
+    'nord-4': {
       base: '#d8dee9',
+    },
+    'nord-5': {
+      base: '#e5e9f0',
+    },
+    'nord-6': {
+      base: '#eceff4',
+    },
+    'nord-7': {
+      base: '#8fbcbb',
+    },
+    'nord-8': {
+      base: '#88c0d0',
+    },
+    'nord-9': {
+      base: '#81a1c1',
+    },
+    'nord-10': {
+      base: '#5e81ac',
+    },
+    'nord-11': {
+      base: '#bf616a',
+    },
+    'nord-12': {
+      base: '#d08770',
+    },
+    'nord-13': {
+      base: '#ebcb8b',
+    },
+    'nord-14': {
+      base: '#a3be8c',
+    },
+    'nord-15': {
+      base: '#b48ead',
+    },
+    primary: {
+      base: '#5e81ac',
+      Content: '#4f6f95',
+    },
+    secondary: {
+      base: '#bf616a',
+      Content: '#ad525b',
+    },
+    'primary-dark': {
+      base: '#88c0d0',
+      Content: '#75afc0',
+    },
+    'secondary-dark': {
+      base: '#bf616a',
+      Content: '#9d4951',
     },
   },
   fontSizes: {},
