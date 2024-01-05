50c70fd621b59a5890f7f85e563355ddcafac8ee
botmaster <pascal.achard@gmail.com>
2024-01-05 10:49:49 +0100

    refactor: Nuxt layouts optimization

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
-const head = useLocaleHead({
+const i18nHead = useLocaleHead({
   addDirAttribute: true,
   addSeoAttributes: true,
+});
+useHead({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang,
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
 });
 </script>
 
 <template>
-  <Html :lang="head.htmlAttrs?.lang" :dir="head.htmlAttrs?.dir">
-    <Head>
-      <template v-for="link in head.link" :key="link.id">
-        <Link
-          :id="link.id"
-          :rel="link.rel"
-          :href="link.href"
-          :hreflang="link.hreflang"
-        />
-      </template>
-      <template v-for="meta in head.meta" :key="meta.id">
-        <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
-      </template>
-    </Head>
-    <Body>
-      <div class="flex flex-col min-h-screen">
-        <TheHeader />
-        <slot />
-        <TheFooter class="mt-auto" />
-      </div>
-    </Body>
-  </Html>
+  <div class="flex flex-col min-h-screen">
+    <TheHeader />
+    <slot />
+    <TheFooter class="mt-auto" />
+  </div>
 </template>
