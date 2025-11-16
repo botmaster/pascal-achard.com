b15ebeac097cbef668ad059eb9e5094d6b0471e5
botmaster <pascal.achard@gmail.com>
2025-11-16 10:59:00 +0100

    fix: update CV redirect path to the latest version

--- server/middleware/redirect-cv.ts
+++ server/middleware/redirect-cv.ts
@@ -4,7 +4,7 @@ export default defineEventHandler((event) => {
   const reqUrl = event.node.req.url ?? '';
 
   // Check if the request URL matches the old CV path
-  if (reqUrl.startsWith('/pdf/CV-Pascal-Achard-20.pdf')) {
+  if (reqUrl.startsWith('/pdf/CV-Pascal-Achard-20')) {
     // permanent redirect to the new path
     return sendRedirect(event, '/pdf/CV-Pascal-Achard.pdf', 301);
   }
