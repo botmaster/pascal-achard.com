425a4ec1cb42cb57ccbda9c2a4b11ac46ffdede3
botmaster <pascal.achard@gmail.com>
2025-11-29 21:13:56 +0100

    fix: adjust camera look-at position and comment out OrbitControls in TheExperience.vue

--- components/tres/TheExperience.vue
+++ components/tres/TheExperience.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { OrbitControls } from '@tresjs/cientos'
+//import { OrbitControls } from '@tresjs/cientos'
 
 import TheDog from './TheDog.vue'
 </script>
@@ -7,9 +7,9 @@ import TheDog from './TheDog.vue'
 <template>
   <TresPerspectiveCamera
     :position="[7, 3, 2]"
-    :look-at="[0, 1, 0]"
+    :look-at="[0, 1, 0.4]"
   />
-  <OrbitControls :target="[0, 1, 0]" />
+<!--  <OrbitControls :target="[0, 1, 0]" />-->
   <TresAmbientLight :intensity="1" />
   <Suspense>
     <Environment preset="sunset" :background="false" :blur="1" />
